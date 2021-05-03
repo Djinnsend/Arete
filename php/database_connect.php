744cfd5fa5aeae6548401feedbcafb11aa640e52
djinnsend <nanaboakye1738@gmail.com>
2021-05-03 01:29:49 +0000

    Edited database credentials

--- php/database_connect.php
+++ php/database_connect.php
@@ -2,11 +2,11 @@
     //This file is solely for connecting to the local database on Xampp
     
     //These are the details for the connection
-    $username="epiz_28524495";
-    $server="sql305.epizy.com";
+    $username="bfd0f7d9f70940";
+    $server="eu-cdbr-west-01.cleardb.com";
     // $pass= getenv('MYSQLPASS'??"");
-    $pass = "Gu1MCmAidHkd";
-    $dbName="epiz_28524495_arete";
+    $pass = "fa79aa69";
+    $dbName="heroku_4cf8f5d4412c58e";
 
     //This is for executing the database connection
     $conn = mysqli_connect($server,$username,$pass,$dbName);
